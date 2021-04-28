5243ace148ba5dec3b8fdbc64b6fb52467910c24
quintin <=>
2021-04-27 21:57:53 -0400

    More auth...no token yet

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 )
+
+var pass string = "0d43a7363c7d2ab93ee6f34f2e2a64b11ed38cb8"
 
 func main() {
 	http.HandleFunc("/", root)
@@ -18,25 +23,38 @@ func root(w http.ResponseWriter, r *http.Request) {
 	url := "http://www.strava.com/oauth/authorize?client_id=18876&response_type=code&redirect_uri=http://localhost:8080/exchangeToken?approval_prompt=force&scope=read,activity:read" //todo get URL!
 
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+
 }
 
 func exchangeToken(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "You are exchanging tokens...good job :)")
-	//	http://localhost:8080/exchangeToken?approval_prompt=force&state=&code=c597abeaba0b5526339e96cec26cd4b815e2eae9&scope=read,activity:read
+	var code string
 	splt := strings.Split(r.URL.RawQuery, "&")
 	for _, j := range splt {
 		t := j[:4]
 		if t == "code" {
 			s := strings.Split(j, "=")
-			fmt.Fprint(w, s[1])
+			code = s[1]
 		}
 	}
-	/*
-		POST to oauth and get bearer token using the code you received from above!
-		curl -X POST https://www.strava.com/oauth/token \
-		-F client_id=YOURCLIENTID \
-		-F client_secret=YOURCLIENTSECRET \
-		-F code=AUTHORIZATIONCODE \
-		-F grant_type=authorization_code
-	*/
+
+	v := url.Values{}
+	v.Set("client_id", "18876")
+	v.Set("client_secret", pass)
+	v.Set("code", code)
+	v.Set("grant_type", "authorization_code")
+
+	client := &http.Client{}
+	log.Print(strings.NewReader(v.Encode()))
+	r, err := http.NewRequest("POST", "https://www.strava.com/oauth/token", strings.NewReader(v.Encode()))
+	if err != nil {
+		log.Print(err)
+	}
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.Header.Add("Content-Length", strconv.Itoa(len(v.Encode())))
+
+	res, err := client.Do(r)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Print(res)
 }
